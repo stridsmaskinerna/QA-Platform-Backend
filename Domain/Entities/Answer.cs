ec317df61478e0826315d48029d5c917f063a135
Joel Martelleur <joel.martelleur@gmail.com>
2025-01-24 11:26:30 +0100

    Merge Dev into Main (#6)
    * Updating compose files.
    
    * Setup Identity framework.
    
    * Update entities.
    
    * Add explicit foreign keys and join tables to entities.
    
    * Create dotnet.yml
    
    * Update build and test workflow.
    
    * Update entities.
    
    * Update entities.
    
    ---------
    
    Co-authored-by: Luca Milanesi <44052150+milanesiluca@users.noreply.github.com>

---
+++ Domain/Entities/Answer.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain.Entities;
+
+public class Answer
+{
+    public Guid Id { get; set; }
+
+    public Guid QuestionId { get; set; }
+
+    public string? UserId { get; set; }
+
+    public string Value { get; set; } = String.Empty;
+
+    public int Rating { get; set; }
+
+    public string? FilePath { get; set; }
+
+    public DateTime Created { get; set; }
+
+    public bool IsHidden { get; set; }
+
+    // Navigation
+
+    public IEnumerable<Comment> Comments { get; set; } = [];
+}
---
+++ Domain/Entities/Topic.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities;
+
+public class Topic
+{
+    public Guid Id { get; set; }
+
+    public Guid SubjectId { get; set; }
+
+    public string Name { get; set; } = String.Empty;
+
+    public bool IsActive{ get; set; }
+
+    // Navigation
+    public IEnumerable<Question> Questions { get; set; } = [];
+
+}
