0d8b5f17972e636cbbdf4ca389b1b94b99204d93
maxth <maxthorstorp@gmail.com>
2025-02-22 14:06:10 +0100

    tidy up

--- Domain/DTO/Request/QuestionForPutDTO.cs
+++ Domain/DTO/Request/QuestionForPutDTO.cs
@@ -7,6 +7,5 @@ public class QuestionForPutDTO
     public string? FileName { get; set; }
     public bool IsProtected { get; set; }
     public Guid TopicId { get; set; }
-    public Guid SubjectId { get; set; }
     public ICollection<string> Tags { get; set; } = [];
 }
